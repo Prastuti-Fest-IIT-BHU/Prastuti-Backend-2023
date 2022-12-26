04f434723340bfab9ceba8528c243cc97b9c6d80
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2022-12-26 15:35:19 +0530

    controller: event,leaderboard,login,score

---
+++ controllers/leaderboard.js
@@ -0,0 +1,58 @@
+const Events = require("../models/Events");
+const Users = require("../models/Users");
+
+//sending info about a particular event "/leaderboard/:name"
+const getEventLeaderboard = async (req, res) => {
+  const eventName = req.params.event;
+  const event = await Event.findOne({ Name: eventName });
+
+  if (!event.team_event) {
+    let data = event.Participants.map((user) => {
+      return {
+        Name: user.participant.Name,
+        College: user.participant.College,
+        Score: user.Score,
+        ProfileImg: user.participant.Profile_Photo,
+      };
+    });
+    data.sort((a, b) => b.Score - a.Score);
+    res.status(200).json({
+      data,
+      teamEvent: false,
+    });
+  } else {
+    let data = event.Teams.map((Team) => {
+      let members = Team.team.Members.map((member) => member.Name);
+      return {
+        Name: Team.team.Team_Name,
+        Score: Team.Score,
+        members,
+      };
+    });
+    data.sort((a, b) => b.Score - a.Score);
+    res.status(200).json({
+      data,
+      teamEvent: true,
+    });
+  }
+};
+
+//sending info when user request at '/leaderboard'
+const getLeaderboard = async (req, res) => {
+  const users = await Users.find({})
+    .sort("-Total_Score")
+    .select("-College -Phone -Pending_Requests -_id -__v -Teams");
+
+  let data = users.map((user) => {
+    return {
+      Name: user.Name,
+      Score: user.Total_Score,
+      College: user.College,
+      NumEvents: user.Events_Participated.length,
+    };
+  });
+
+  res.status(200).json({ data });
+};
+
+module.exports = { getLeaderboard, getEventLeaderboard };
---
+++ controllers/login.js
@@ -0,0 +1,49 @@
+const { response } = require("express");
+const res = require("express/lib/response");
+const { OAuth2Client } = require("google-auth-library");
+
+const User = require("../models/Users");
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const loginUser = async (req, res) => {
+  const tokenId = req.body.tokenId;
+  let isNew = false;
+  let user = {};
+
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+      idToken: tokenId,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const curUser = await Users.findOne({
+      email_id: payload.email,
+    });
+
+    if (!curUser) {
+      isNew = true;
+      let newUser = {
+        Name: payload.name,
+        email_id: payload.email,
+        Profile_Photo: payload.picture,
+        Teams: [],
+        Pending_Requests: [],
+        Events_Participated: [],
+      };
+      user = await Users.create(newUser);
+    } else user = curUser;
+  }
+
+  
+  verify().then(() => {
+    res.json({
+      message: "Success",
+      user,
+      isNew,
+    });
+  });
+};
+
+module.exports = { loginUser };
