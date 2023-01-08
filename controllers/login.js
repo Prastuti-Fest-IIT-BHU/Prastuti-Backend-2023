f7feffabbe6ba06e6d51e2dbd355754e6fded584
rahul-X01 <rahul.kumar.eee21@gmail.com>
2023-01-08 23:46:06 +0530

    making some changes

--- controllers/login.js
+++ controllers/login.js
@@ -2,49 +2,57 @@ const { response } = require("express");
 const res = require("express/lib/response");
 const { OAuth2Client } = require("google-auth-library");
 
-const User = require("../models/Users");
+const Users = require("../models/Users");
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const loginUser = async (req, res) => {
+
+const loginUser = async (req, res) =>{
   const tokenId = req.body.tokenId;
-  let isNew = false;
+  let isNew = false; 
   let user = {};
-
   async function verify() {
-    const ticket = await client.verifyIdToken({
-      idToken: tokenId,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
+      const ticket = await client.verifyIdToken({
+          idToken: tokenId,
+          audience: process.env.GOOGLE_CLIENT_ID
+      })
+      const payload = ticket.getPayload();
+      console.log(payload);
+      
 
-    const payload = ticket.getPayload();
-    const curUser = await Users.findOne({
-      email_id: payload.email,
-    });
+      const curUser = await Users.findOne({
+          email_id: payload.email
+      })
 
-    if (!curUser) {
-      isNew = true;
-      let newUser = {
-        Name: payload.name,
-        email_id: payload.email,
-        Profile_Photo: payload.picture,
-        App_id: "",
-        Teams: [],
-        Pending_Requests: [],
-        Events_Participated: [],
-      };
-      user = await User.create(newUser);
-    } else user = curUser;
+      if(!curUser) {
+          isNew = true;
+          let newUser = {
+              Name: payload.name,
+              email_id: payload.email,
+              Profile_Photo: payload.picture,
+              Teams: [],
+              Pending_Requests: [],
+              Events_Participated: []
+          }
+          user = await Users.create(newUser);
+      }
+      else user = curUser;
   }
+
 
   
   verify().then(() => {
     res.json({
-      message: "Success",
-      user,
-      isNew,
-    });
-  });
-};
+        message: 'Success',
+        user,
+        isNew
+    })
+}).catch(err => 
+  {  console.log("error bro");
+    res.json({
+    message: 'Fail bro',
+    error: err
+})}) 
+}
 
-module.exports = { loginUser };
+module.exports = {loginUser};
